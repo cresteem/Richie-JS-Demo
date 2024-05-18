26d052237b1ef70cc0b97f13b6c0fe5b9257e7ef
iamspdarsan <iamspdarsan@gmail.com>
2024-05-18 16:16:02 +0530

    initial setup

---
+++ demo.cjs
@@ -0,0 +1,2 @@
+const { richie } = require("@cresteem/richie-js");
+richie("article", "pages/article.html", "pages/article.htm");
---
+++ demo.mjs
@@ -0,0 +1,6 @@
+import { richie } from "@cresteem/richie-js";
+richie(
+  "breadcrumb",
+  "pages/breadcrumb/sub-breadcrumb/notindex.html",
+  "pages/bc.html"
+);
