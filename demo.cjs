28d8c8ce26f0839ecdd31d3f31a6aa71121010d7
iamspdarsan <iamspdarsan@gmail.com>
2024-05-25 10:54:28 +0530

    demo updated for 1.1.1

--- demo.cjs
+++ demo.cjs
@@ -1,8 +1,16 @@
 const { richie } = require("@cresteem/richie-js");
-const { basename, join } = require("path");
+const { mkdirSync } = require("fs");
+const { basename, join, dirname, relative } = require("path");
 
-const type = "article";
-const inputFile = "pages/article.html";
-const outputFile = join("./dist", basename(inputFile));
+const type = "searchbox";
+const inputFile = "pages/Sitesearch/searchpage.html";
+const outputFile = join(
+  "./dist",
+  relative(process.cwd(), dirname(inputFile)),
+  basename(inputFile)
+);
+
+//making dest folder
+mkdirSync(dirname(outputFile), { recursive: true });
 
 richie(type, inputFile, outputFile);
--- demo.mjs
+++ demo.mjs
@@ -1,8 +1,16 @@
 import { richie } from "@cresteem/richie-js";
-import { basename, join } from "path";
+import { mkdirSync } from "fs";
+import { basename, join, relative, dirname } from "path";
 
 const type = "breadcrumb";
 const inputFile = "pages/breadcrumb/sub-breadcrumb/notindex.html";
-const outputFile = join("./dist", basename(inputFile));
+const outputFile = join(
+  "./dist",
+  relative(process.cwd(), dirname(inputFile)),
+  basename(inputFile)
+);
+
+//making dest folder
+mkdirSync(dirname(outputFile), { recursive: true });
 
 richie(type, inputFile, outputFile);
